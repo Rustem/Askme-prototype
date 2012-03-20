4ced63324cb6fb467dbd00cff348e2fc954ea0e6
Vadim Kotov <igeeko@gmail.com>
2012-03-20 16:07:14 +0600

    Appearance editing, almost done 	modified:   create survey.html 	modified:   javascripts/app.js 	modified:   stylesheets/app.css

--- javascripts/app.js
+++ javascripts/app.js
@@ -91,5 +91,23 @@ jQuery(document).ready(function ($) {
 	/* DISABLED BUTTONS ------------- */
 	/* Gives elements with a class of 'disabled' a return: false; */
   
+  
+  	/* ACCORDION */
+  	(function($) {
+  		var allPanels = $('.accordion > dd').hide();
+  		
+  		$('.accordion > dt > a').click(function() {
+  			$this = $(this);
+  			$target = $this.parent().next();
+  			
+  			if (!$target.hasClass('active')) {
+  				allPanels.removeClass('active').slideUp();
+  				$target.addClass('active').slideDown();
+  			}
+  			
+  			return false;
+  		});
+  		
+  	})(jQuery);
 
 });
